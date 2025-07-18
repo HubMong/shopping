501a08a0701dbb6f1a68d3cca037810e5dfadb19
hwi0-02 <167694199+hwi0-02@users.noreply.github.com>
2025-07-18 15:36:28 +0900

    Add files via upload

---
+++ shopping/src/main/java/mapper/OrderMapper.java
@@ -0,0 +1,13 @@
+package mapper;
+
+import model.Order;
+import model.OrderItem;
+import java.util.List;
+
+public interface OrderMapper {
+    void insertOrder(Order order);
+    void insertOrderItem(OrderItem orderItem);
+    List<OrderItem> selectOrderItemsByOrderId(int orderId);
+    List<Order> selectOrdersByMemberId(String memberId);
+    void deleteOrderItemsByBookId(int bookId);
+}
